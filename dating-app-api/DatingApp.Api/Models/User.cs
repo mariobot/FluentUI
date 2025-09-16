af03f0320c6c0c63f2f7b1b51c3453b2a3c8c726
mariobot <mariobot113@hotmail.com>
2025-09-16 11:53:40 -0500

    update project

---
+++ dating-app-api/DatingApp.Api/Models/User.cs
@@ -0,0 +1,12 @@
+namespace DatingApp.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string? Bio { get; set; }
+        public string? PhotoUrl { get; set; }
+    }
+}
